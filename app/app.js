b6fba6c6bfb22c68dc3356008951dd80c48150a9
sideral <a31415926535@gmail.com>
2015-04-06 05:08:23 -0500

    Relocated files

--- app/app.js
+++ app/app.js
@@ -5,13 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var pages = require('./pages/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'pages/views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', pages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
