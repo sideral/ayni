3eaf9eda7b598e74c65be2dba3c313b0261bb181
sideral <a31415926535@gmail.com>
2015-04-06 06:07:26 -0500

    Reorganized files

--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var pages = require('./pages/index');
+var pages = require('../pages/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'pages/views'));
+app.set('views', path.join(__dirname, '../pages/views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', pages);
 
