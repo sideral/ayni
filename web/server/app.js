e3e9a1147f7becc7e05b5f081c9e3f2346fcf8ef
sideral <a31415926535@gmail.com>
2015-04-16 17:02:37 -0500

    Installed express again.

--- web/server/app.js
+++ web/server/app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./config/routes');
+var routes = require('./routes/index');
+var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './views/pages'));
+app.set('views', path.join(__dirname, '../client/views'));
 app.set('view engine', 'hbs');
-app.set('view options', { layout: 'layouts/main' });
+app.set('view options', {layout: 'layouts/main'});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,9 +21,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
